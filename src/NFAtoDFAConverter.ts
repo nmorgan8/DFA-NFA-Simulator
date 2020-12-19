505fa8c524a166e501bdf50a153e4081c7676297
Nick Morgan <nmorgan8@lion.lmu.edu>
2020-12-18 16:49:32 -0800

    initial work on nfa to dfa converter

--- src/NFAtoDFAConverter.ts
+++ src/NFAtoDFAConverter.ts
@@ -8,6 +8,8 @@ export function NFAtoDFAConverter(
   nfa: NonDeterministicFiniteStateMachine
 ): DeterministicFiniteStateMachine {
   const nfaDesc = nfa.getDesc();
+
+  // if no transtition for symbol 0/1 add empty transition
   for (const stateTrans of Object.values(nfaDesc.transitions)) {
     for (const state of Object.keys(stateTrans)) {
       console.log(`state: ${state}\ttransitions: ${stateTrans}`);
@@ -22,6 +24,7 @@ export function NFAtoDFAConverter(
     }
   }
 
+  // init new dfa
   const dfa = {
     transitions: {},
     start: null,
@@ -29,9 +32,17 @@ export function NFAtoDFAConverter(
   };
   console.log(nfaDesc);
 
+  // init dead state with transitions to self
   const dead = 'dead';
   nfaDesc.transitions[dead] = { 0: [dead], 1: [dead] };
   const result = new DeterministicFiniteStateMachine(dfa);
+
+  // stack to track which states we still need to convert
+  let frontier = [[nfaDesc.start]];
+
+  while (frontier.length > 0) {
+    let currStates = frontier.pop();
+  }
 
   return result;
 }
